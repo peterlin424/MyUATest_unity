5c0dd7b0dc13927561c093e88bf71f99c2094fe1
peterlin424 <peterlin424@gmail.com>
2016-03-25 18:42:18 +0800

    test ver 1

---
+++ Assets/Scripts/page1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class page1 : MonoBehaviour {
+
+	private string pageName = "page1";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ButtonClickEvent(string bt_name){
+		Debug.Log (pageName + " : " + bt_name);
+		switch (bt_name) {
+		case "bt_back":
+			break;
+		case "bt_next":
+			break;	
+		}
+	}
+}
---
+++ Assets/Scripts/page2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class page2 : MonoBehaviour {
+
+	private string pageName = "page2";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ButtonClickEvent(string bt_name){
+		Debug.Log (pageName + " : " + bt_name);
+		switch (bt_name) {
+		case "bt_back":
+			break;
+		case "bt_finish":
+			break;	
+		}
+	}
+}
